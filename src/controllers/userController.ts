7abd7419c5046d30863dbfa7fb22598d6c474778
bababuuui <bubinatar@gmail.com>
2022-06-15 17:42:00 +0200

    fix - user controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,10 +1,12 @@
 import { IncomingMessage, ServerResponse } from "http";
+import * as uuid from "uuid";
 import Users from "../models/userModel";
+import { JSON_CONTENT_TYPE } from "../constants/headers";
 
 async function getUsers(req: IncomingMessage, res: ServerResponse) {
   try {
     const users = await Users.getAllUsers();
-    res.writeHead(200, { "Content-Type": "application/json" });
+    res.writeHead(200, JSON_CONTENT_TYPE);
     res.end(JSON.stringify(users));
   } catch (error) {
     console.log(error);
@@ -13,13 +15,18 @@ async function getUsers(req: IncomingMessage, res: ServerResponse) {
 
 async function getUser(req: IncomingMessage, res: ServerResponse, id: string) {
   try {
-    const user = await Users.findUserById(id);
-    if (user) {
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(JSON.stringify(user));
+    if (uuid.validate(id)) {
+      const user = await Users.findUserById(id);
+      if (user) {
+        res.writeHead(200, JSON_CONTENT_TYPE);
+        res.end(JSON.stringify(user));
+      } else {
+        res.writeHead(404, JSON_CONTENT_TYPE);
+        res.end("User not found");
+      }
     } else {
-      res.writeHead(404, { "Content-Type": "application/json" });
-      res.end("User not found");
+      res.writeHead(400, JSON_CONTENT_TYPE);
+      res.end("userId is invalid (not uuid)");
     }
   } catch (error) {
     console.log(error);
