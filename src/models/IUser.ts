13fe31cd59d1c557daf69153a450d18f84cbaff7
bababuuui <bubinatar@gmail.com>
2022-06-15 22:35:06 +0200

    feat - add type check for User

--- src/models/IUser.ts
+++ src/models/IUser.ts
@@ -1,6 +1,10 @@
 export interface IUser {
-  id: string;
+  id?: string;
   username: string;
   age: number;
   hobbies: string[];
+}
+
+export function isUser(obj: any): obj is IUser {
+  return "username" in obj && "age" in obj && "hobbies" in obj;
 }
