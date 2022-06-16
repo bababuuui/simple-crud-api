070a5108c166ccc83e9bf1f2b6ff597de02a9543
bababuuui <bubinatar@gmail.com>
2022-06-16 17:37:48 +0200

    feat - move server to index js and implement workers

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,25 @@
 import dotenv from "dotenv";
-import { server } from "./server";
-
-dotenv.config();
-
-console.log(process.env.MY_VAR1);
+import cluster from "cluster";
+import * as os from "os";
+import http from "http";
+import { route } from "./router/router";
 
 const port = process.env.PORT || 3000;
-
-server.listen(port, () => {
-  console.log(`Server running at port ${port}`);
-});
+const { pid } = process;
+if (cluster.isPrimary) {
+  dotenv.config();
+  const count = process.env.LOAD_BALANCING === "true" ? os.cpus().length : 1;
+  console.log(`Master pid ${pid}`);
+  console.log(`Starting forks ${count}`);
+  for (let i = 0; i < count; i++) cluster.fork();
+} else {
+  const { id } = cluster.worker;
+  http
+    .createServer(async (req, res) => {
+      res.setHeader("Process-Id", pid);
+      await route(req, res, id);
+    })
+    .listen(port, () => {
+      console.log(`Worker: ${id}, pid: ${pid}, port: ${port}`);
+    });
+}
